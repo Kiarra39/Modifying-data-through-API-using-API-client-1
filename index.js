c0ec684414e78e4369ebb9c62f7db2f355b9e7b1
Kiesha Maria <kiesha.maria@kalvium.community>
2025-01-08 15:03:24 +0530

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express = require('express');
+require('dotenv').config();
 const { resolve } = require('path');
+const mongoose= require('mongoose');
+const MenuItem= require ('./schema.js');
 
 const app = express();
+
 const port = 3010;
 
-app.use(express.static('static'));
+app.use(express.json());
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => console.error('MongoDB connection error:', err));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+app.post('/menu', async(req,res)=>{
+  try{
+    const {name, description, price}= req.body;
+    if (!name || price == null) {
+      return res.status(400).json({ error: 'Name and price are required.' });
+  }
+  const newItem=new MenuItem({name,description,price});
+  const createdItem= await newItem.save();
+  res.status(201).json({ message: 'Menu item created successfully.', item: createdItem });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to create menu item.' });
+  }
+})
+app.get('/menu', async (req, res) => {
+  try {
+    const items = await MenuItem.find();
+    res.status(200).json(items);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch menu items.' });
+  }
 });
 
 app.listen(port, () => {
